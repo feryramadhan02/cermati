39e4970320668720d60f8fde9d2a96bdbed9acde
feryrmdhnc <feryramadhan555@gmail.com>
2020-08-22 14:44:05 +0800

    add icon

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { data } from '../data/data'
 
+
 import '../assets/style/card.scss'
 import '../assets/style/responsive.scss'
 
@@ -12,6 +13,7 @@ const Cards = () => {
                     <div key={item.id} className="base__card">
                         <div className="base__card__icon">
                             <h4>{item.title}</h4>
+                            <img src={item.image} alt="" />
                         </div>
                         <p>{item.desc}</p>
                     </div>
